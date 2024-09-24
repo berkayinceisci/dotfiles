290515e17c8d8afd358f0b0b1a608c1c62c5a2f0
inceisciberkay <inceisciberkay@gmail.com>
2024-09-24 23:46:55 +0300

    fix transparency

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -56,3 +56,6 @@ opt.foldcolumn = '1' -- '0' is not bad
 opt.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
 opt.foldlevelstart = 99
 opt.foldenable = true
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
