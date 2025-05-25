6df655d6403ddb50415b00f3f89366f0e5abd883
inceisciberkay <inceisciberkay@gmail.com>
2025-05-25 10:11:54 +0000

    nvim fix C-BSpace bug

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,7 +1,7 @@
 local keymap = vim.keymap
 
 keymap.set("i", "<C-w>", "<nop>")
-keymap.set("i", "<C-H>", "<Esc>ciw", { noremap = true }) -- C-Backspace
+keymap.set("i", "<C-H>", "<C-o>dB", { noremap = true }) -- C-Backspace
 
 keymap.set("n", "<C-w>%", "<C-w>v") -- split window vertically
 keymap.set("n", '<C-w>"', "<C-w>s") -- split window horizontally
