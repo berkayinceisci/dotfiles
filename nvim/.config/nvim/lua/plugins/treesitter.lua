4b1534b2a1d6149c1bbc0f5ccac2c0969f856e61
inceisciberkay <inceisciberkay@gmail.com>
2025-07-06 06:48:01 +0000

    nvim: treesitter remove assignment left/write selection to prevent delay on visual selection movementsj

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -45,8 +45,9 @@ return {
 					keymaps = {
 						["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment" },
 						["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment" },
-						["l="] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
-						["r="] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
+						-- below keymaps cause delay in visual selection movement
+						-- ["l="] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
+						-- ["r="] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
 
 						["a;"] = { query = "@statement.outer", desc = "Select outer part of a statement" },
 
