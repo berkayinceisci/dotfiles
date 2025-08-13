b454b8b96f43e8e510990087b254d06f9c7e1167
inceisciberkay <inceisciberkay@gmail.com>
2025-08-13 10:04:28 -0400

    update i3 config

--- i3/.config/i3/display_setup.sh
+++ i3/.config/i3/display_setup.sh
@@ -2,7 +2,7 @@
 
 # Check if HDMI-1-1 is connected, if so make the external monitor primary
 if xrandr | grep "^HDMI-1-0 connected"; then
-    xrandr --output HDMI-1-0 --mode 2560x1440 --rate 59.95 --right-of eDP-1
+    xrandr --output HDMI-1-0 --mode 2560x1440 --rate 120.00 --right-of eDP-1
 elif xrandr | grep "^HDMI-1-1 connected"; then
-    xrandr --output HDMI-1-1 --mode 2560x1440 --rate 59.95 --right-of eDP-1
+    xrandr --output HDMI-1-1 --mode 2560x1440 --rate 120.00 --right-of eDP-1
 fi
