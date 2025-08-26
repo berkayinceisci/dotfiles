9d281492e0f9c274f1228e0e055a6e58e2729099
inceisciberkay <inceisciberkay@gmail.com>
2025-08-26 17:44:40 -0400

    update i3 config && add .Xresources for dpi scaling

--- i3/.config/i3/display_setup.sh
+++ i3/.config/i3/display_setup.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-# Check if HDMI-1-1 is connected, if so make the external monitor primary
-if xrandr | grep "^HDMI-1-0 connected"; then
-    xrandr --output HDMI-1-0 --mode 2560x1440 --rate 120.00 --right-of eDP-1
-elif xrandr | grep "^HDMI-1-1 connected"; then
-    xrandr --output HDMI-1-1 --mode 2560x1440 --rate 120.00 --right-of eDP-1
-fi
+HOST=$(hostname)
+
+case "$HOST" in
+    "berkay") # manjaro and ubuntu
+        if xrandr | grep "^HDMI-1-0 connected" > /dev/null; then
+            xrandr --output HDMI-1-0 --mode 2560x1440 --rate 120.00 --right-of eDP-1
+        elif xrandr | grep "^HDMI-1-1 connected" > /dev/null; then
+            xrandr --output HDMI-1-1 --mode 2560x1440 --rate 120.00 --right-of eDP-1
+        fi
+        ;;
+    "pop-os")
+        if xrandr | grep "^HDMI-1 connected" > /dev/null; then
+            xrandr --output HDMI-1 --mode 3840x2160 --rate 60.00
+        elif xrandr | grep "^HDMI-2 connected" > /dev/null; then
+            xrandr --output HDMI-2 --mode 3840x2160 --rate 60.00
+        ;;
+esac
