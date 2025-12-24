b871f0e586759f8e4c8fc96bbdc8d313ea481689
berkayinceisci <inceisciberkay@gmail.com>
2025-12-24 15:47:41 -0500

    update i3lock

---
+++ i3/.config/i3/lock.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+BLANK='#00000000'
+CLEAR='#ffffff22'
+DEFAULT='#ffffff88'
+TEXT='#ffffffbb'
+WRONG='#880000bb'
+VERIFYING='#9b8361ff'
+
+i3lock \
+--insidever-color=$CLEAR     \
+--ringver-color=$VERIFYING   \
+\
+--insidewrong-color=$CLEAR   \
+--ringwrong-color=$WRONG     \
+\
+--inside-color=$BLANK        \
+--ring-color=$DEFAULT        \
+--line-color=$BLANK          \
+--separator-color=$DEFAULT   \
+\
+--verif-color=$TEXT          \
+--wrong-color=$TEXT          \
+--time-color=$TEXT           \
+--date-color=$TEXT           \
+--layout-color=$TEXT         \
+--keyhl-color=$WRONG         \
+--bshl-color=$WRONG          \
+\
+--blur 5                     \
+--clock                      \
+--indicator                  \
+--time-str="%H:%M:%S"        \
+--date-str="%A, %Y-%m-%d"       \
+--keylayout 1                \
+--radius 120                    \
+--ring-width 12
