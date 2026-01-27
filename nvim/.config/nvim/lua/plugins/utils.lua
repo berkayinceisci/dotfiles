21db90773b3d796f7777a182e75cd16da5c33228
berkayinceisci <inceisciberkay@gmail.com>
2026-01-27 15:20:11 -0500

    up leap

--- nvim/.config/nvim/lua/plugins/utils.lua
+++ nvim/.config/nvim/lua/plugins/utils.lua
@@ -59,7 +59,7 @@ return {
 		end,
 	},
 	{
-		"ggandor/leap.nvim",
+		"https://codeberg.org/andyg/leap.nvim",
 		config = function()
 			vim.keymap.set({ "n", "x", "o" }, "<leader>w", "<Plug>(leap-forward)")
 			vim.keymap.set({ "n", "x", "o" }, "<leader>b", "<Plug>(leap-backward)")
