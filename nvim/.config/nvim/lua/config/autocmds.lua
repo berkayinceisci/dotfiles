8d5c3420006002e3009fbb70e313fb9fe4533c23
berkayinceisci <inceisciberkay@gmail.com>
2026-02-05 17:58:30 -0500

    nvim add spelling for md and tex files

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -8,6 +8,15 @@ autocmd({ "FileType" }, {
 	end,
 })
 
+-- Enable spellcheck for markdown and LaTeX files
+autocmd({ "FileType" }, {
+	pattern = { "markdown", "tex" },
+	callback = function()
+		vim.opt_local.spell = true
+		vim.opt_local.spelllang = "en_us"
+	end,
+})
+
 -- Auto-reload files when changed externally (e.g., by Claude Code)
 vim.opt.autoread = true
 autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
