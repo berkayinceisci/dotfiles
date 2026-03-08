2347bc47219e59fed8a092b006b608dfa70a26dc
berkayinceisci <inceisciberkay@gmail.com>
2026-03-08 04:26:51 -0400

    wezterm increase scrollback lines

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -108,6 +108,8 @@ config.window_padding = {
 config.hide_tab_bar_if_only_one_tab = false
 config.use_fancy_tab_bar = false
 
+config.scrollback_lines = 50000
+
 config.enable_kitty_keyboard = false
 
 config.keys = {
